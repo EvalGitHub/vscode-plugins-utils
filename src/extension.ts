aaffe2d3b2591979db142a06c1777de830e3a050
evel <957010831@qq.com>
2025-08-02 22:59:51 +0800

    feat: 目录结构调整

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {ViewDepCode} from './command/project-dep/view-dep-code';
+import {ViewDepCode} from './command/project-dep';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
