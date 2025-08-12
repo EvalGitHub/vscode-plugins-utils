519d734f8391afe0cdee1a8ab9361f653cadae3f
evel <957010831@qq.com>
2025-08-12 16:13:13 +0800

    feat: 实现side-bar view 的开发

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,41 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {ViewDepCode} from './command/project-dep';
+import {CodeAiByQwenSideBarViewProvider} from './command/code-ai-by-qwen';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
 	// 项目依赖处理插件
-	const ViewDepCodeProvider = new ViewDepCode(context.extensionUri as vscode.Uri);
+	function getViewDepCodeProvider() {
+		const ViewDepCodeProvider = new ViewDepCode(
+			context.extensionUri as vscode.Uri,
+			"index.html"
+		);
+		return  vscode.commands.registerCommand("view-project-dep-source-code", (e) => {
+      console.log(e.fsPath);
+      ViewDepCodeProvider.getProjectDeps(e.fsPath);
+    });
+	}
+
+
+  // 注册 code-ai-by-qwen sideBar view
+	function getCodeAiByQwenSideBarViewProvider() {
+		const provider = new CodeAiByQwenSideBarViewProvider(
+      context.extensionUri as vscode.Uri,
+			"code-ai-by-qwen.html"
+		);
+		return vscode.window.registerWebviewViewProvider(
+      "code-ai-by-qwen-container",
+      provider
+    );
+	}
+
 	context.subscriptions.push(
-		vscode.commands.registerCommand("view-project-dep-source-code", (e) => {
-			console.log(e.fsPath);
-			ViewDepCodeProvider.getProjectDeps(e.fsPath);
-		}));
+    getViewDepCodeProvider(),
+    getCodeAiByQwenSideBarViewProvider()
+  );
 }
 
 // This method is called when your extension is deactivated
---
+++ src/vite-project/src/code-ai-by-qwen/App.jsx
@@ -0,0 +1,14 @@
+ 
+
+import './App.css'
+
+function App() {
+
+  return (  
+    <div>
+      this is code-ai-by-qwen html
+    </div> 
+  )
+}
+
+export default App
